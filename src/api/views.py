4be987d785580f38be10086a889657293581848b
Kaikaikaifang <2837968358@qq.com>
2023-08-22 13:03:08 +0800

    Update views.py

--- src/api/views.py
+++ src/api/views.py
@@ -15,7 +15,7 @@ import json
 
 
 path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data.json')
-
+times = 3
 
 @api.get('/')
 def index():
@@ -40,7 +40,7 @@ def get_data():
     with open(path, 'r', encoding="utf-8") as f:
         try:
             data=json.load(f)
-            if data[-1]['times'] < 5:
+            if data[-1]['times'] < times:
                 data.pop()
             data.reverse()        
         except json.JSONDecodeError:
@@ -53,7 +53,7 @@ def save_data(data: str):
             existing_data = json.load(f)
         except json.JSONDecodeError:
             existing_data = []
-    if existing_data and existing_data[-1]['times'] < 5:
+    if existing_data and existing_data[-1]['times'] < times:
         existing_data[-1]['times'] += 1
         existing_data[-1]['data'] = existing_data[-1]['data'][:-6] + data
         existing_data[-1]['datetime'] = utils.getTs()
@@ -61,18 +61,19 @@ def save_data(data: str):
         existing_data.append({'times': 1, 'data': data, 'datetime': utils.getTs()})    
     with open(path, 'w') as file:
         json.dump(existing_data, file, indent=4)
-    return existing_data[-1]['times'] == 5
+    return existing_data[-1]['times'] == times
 
 def analyze_data(origin_data: list):
     analyzed_data = []
     for obj in origin_data:
-        obj['SBP'] = int(obj['data'][1000:1002], 16)
-        obj['DBP'] = int(obj['data'][1002:1004], 16)
-        obj['HR'] = int(obj['data'][1004:1006], 16) 
+        index = times * 200
+        obj['SBP'] = int(obj['data'][index:index+2], 16)
+        obj['DBP'] = int(obj['data'][index+2:index+4], 16)
+        obj['HR'] = int(obj['data'][index+4:index+6], 16) 
         obj['PPG'] = []
         obj['ECG'] = []
         index = 0
-        for t in range(5):
+        for t in range(times):
             for i in range(t*200, t*200+100, 4):
                 obj['PPG'].append([index, int(obj['data'][i:i+2], 16)*256 + int(obj['data'][i+2:i+4], 16)])
                 obj['ECG'].append([index, int(obj['data'][i+100:i+102], 16)*256 + int(obj['data'][i+102:i+104], 16)])
